1b79d41c7bd89b96b0e8a8dd5e10cc0430e67734
adellylima <adellylima@hotmail.com>
2017-12-11 01:57:47 -0300

    upes

--- app1/modules/commons/controllers/main.Ctrl.js
+++
@@ -1,7 +0,0 @@
-function MainCtrl($rootScope) {
-    var vm = this;
-    vm.usuario = $rootScope.usuario;
-}
-
-angular.module('appList')
-        .controller('MainCtrl', MainCtrl);
--- app1/modules/commons/factories/main.js
+++
@@ -1,9 +0,0 @@
-function MainFactory(){
-
-    var factory = {};
-
-    return factory;
-}
-
-angular.module("appList")
-        .factory("MainFactory",MainFactory);
